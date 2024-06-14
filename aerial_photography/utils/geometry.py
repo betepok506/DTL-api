c4c78202ac2b8e17746b5e2eac5cb8a580a843aa
kuman <d.kumankin@gmail.com>
2024-06-14 12:57:48 +0300

    add convert_wkb_to_coordinates

--- aerial_photography/utils/geometry.py
+++ aerial_photography/utils/geometry.py
@@ -6,7 +6,10 @@ from shapely.geometry import Polygon
 from typing import List, Tuple
 import shapely
 import shapely.wkt
+import shapely.wkb
+from shapely.geometry import mapping
 from geoalchemy2.elements import WKBElement
+
 
 
 def convert_polygon_to_str(polygon_coordinates: List[Tuple[float, float]]):
@@ -33,3 +36,19 @@ def convert_str_to_wkb(str_polygon: str) -> WKBElement:
 
 def convert_wkb_to_str(wkb: WKBElement) -> str:
     return str(to_shape(wkb))
+
+def convert_wkb_to_coordinates(wkb: WKBElement):
+    geom = shapely.wkb.loads(wkb)
+
+    # Получение координат углов
+    coords = list(mapping(geom)['coordinates'][0])
+
+    # Упрощение координат до углов (верхний левый, верхний правый, нижний правый, нижний левый)
+    ul, ur, br, bl = coords[0], coords[1], coords[2], coords[3]
+
+    return {
+        "ul": ul,
+        "ur": ur,
+        "br": br,
+        "bl": bl
+    }
